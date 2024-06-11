71bef7d66f35fa737bcec2ba989ad02d61655c89
ChrisDCP <christopherdarielc@gmail.com>
2024-06-11 14:20:12 -0600

    Iniciando proyecto, dependencias de nav, estructura de main/app

---
+++ Main.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Navegacion from './Navegacion'
+
+export default function Main() {
+  return (
+    <View>
+      <Navegacion/>
+    </View>
+  )
+}
